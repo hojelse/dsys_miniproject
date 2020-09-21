b7556ecf59b27511b2d2e571c14f13c5fb7d2e06
tjoms tjomson <thhk@itu.dk>
2020-09-21 11:08:28 +0200

    Restructure folders

---
+++ Mini Project 1/3/ReliableUDPClient.java
@@ -0,0 +1,112 @@
+package ReliableUDPSystem;
+
+import java.io.IOException;
+import java.net.BindException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import java.util.Scanner;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ReliableUDPClient {
+    private static DatagramSocket socket = null;
+    private static String serverIP = null;
+    private static int serverPort = 7007;
+    private static InetAddress serverAdress;
+    private static int tries;
+
+    public static void main(String[] args) {
+
+        // Ask for port
+        Scanner sc = new Scanner(System.in);
+        while (socket == null) {
+            try {
+                System.out.println("Choose a port:");
+                int incomingPort = sc.nextInt();
+                sc.nextLine();
+                if (incomingPort > 1024) {
+                    socket = new DatagramSocket(incomingPort);
+                    socket.setSoTimeout(200);
+                }
+                System.out.println("");
+            } catch (BindException e) {
+                System.out.println("Port already bound, try another");
+            } catch (Exception e) {
+                e.printStackTrace();
+                sc.close();
+                return;
+            }
+        }
+
+        // Ask for ip
+        while (serverIP == null) {
+            System.out.println("Choose a destination ip:");
+            String input = sc.nextLine();
+
+            String regex = "^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(input);
+
+            boolean matchFound = matcher.find();
+            if (matchFound) {
+                serverIP = input;
+            } else {
+                System.out.println("Not a valid ip");
+            }
+            System.out.println("");
+        }
+
+        try {
+            serverAdress = InetAddress.getByName(serverIP);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Ask for message
+        String msg;
+        do {
+            System.out.println("Type a message..");
+            msg = sc.nextLine();
+        } while (msg.length() > 255);
+        sc.close();
+
+        // Generate UUID
+
+        UUID uuid = UUID.randomUUID();
+        String msgid = uuid.toString();
+
+        String msgWithId = msgid + msg;
+
+        byte[] msgArray = msgWithId.getBytes();
+        DatagramPacket packet = new DatagramPacket(msgArray, msgArray.length, serverAdress, serverPort);
+
+        while (tries < 10) {
+            try {
+                socket.send(packet);
+
+                // Receive
+                byte[] receivedBuffer = new byte[1000];
+                DatagramPacket receivedPacket = new DatagramPacket(receivedBuffer, receivedBuffer.length);
+                socket.receive(receivedPacket);
+
+                String ack = new String(receivedPacket.getData()).trim();
+
+                if (ack.equals(msgid)) {
+                    System.out.println("Success, received acknowledgment from server");
+                    System.exit(0);
+                }
+            } catch (SocketTimeoutException ste) {
+                tries++;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Failed, didn't receive an acknowledgement from server after 10 tries");
+        System.exit(0);
+
+    }
+}
---
+++ Mini Project 1/3/ReliableUPDServer.java
@@ -0,0 +1,53 @@
+package ReliableUDPSystem;
+
+import java.net.*;
+import java.util.HashSet;
+import java.io.*;
+
+public class ReliableUPDServer {
+    private static int incomingPort = 7007;
+    private static HashSet<String> msgIds = new HashSet<>();
+
+    public static void main(String args[]) {
+
+        // Open socket
+        DatagramSocket socket = null;
+        try {
+            socket = new DatagramSocket(incomingPort);
+            System.out.println("Server listening on port " + incomingPort);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            while (true) {
+                byte[] receivedBuffer = new byte[1000];
+                DatagramPacket receivedPacket = new DatagramPacket(receivedBuffer, receivedBuffer.length);
+                socket.receive(receivedPacket);
+
+                int idLength = 36;
+
+                String receivedMsg = new String(receivedPacket.getData()).trim();
+
+                String id = receivedMsg.substring(0, idLength);
+                String msg = receivedMsg.substring(idLength, receivedMsg.length());
+
+                // Check msgid set before print
+                if (msgIds.contains(id))
+                    continue;
+                msgIds.add(id);
+                System.out.println(msg);
+
+                // Send acknowledgment
+                byte[] sendBuffer = id.getBytes();
+                DatagramPacket ackPacket = new DatagramPacket(sendBuffer, sendBuffer.length,
+                        receivedPacket.getAddress(), receivedPacket.getPort());
+                socket.send(ackPacket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            socket.close();
+        }
+    }
+}
